46dede6d975dac29a6bb2cab5110ce34e5421078
rober <robert.nunes@evo1.com.br>
2021-09-22 00:31:22 -0300

    styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {CdkDragDrop, moveItemInArray} from "@angular/cdk/drag-drop";
+import {MatIconRegistry} from "@angular/material/icon";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -8,4 +11,48 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'desafio-airbnb-jurema';
   tags = [{name: 'Dates'}, {name: 'Guests'}, {name: 'Work trip'}, {name: 'Type of place'}, {name: 'Price'}, {name: 'Instant Book'}];
+  card = [
+    {
+      plus: true,
+      image: 'https://a0.muscache.com/im/pictures/c9c9666d-b703-4814-9db6-de91c3b682a0.jpg?im_w=480',
+      type: 'Entire guesthouse',
+      title: 'Charmant petit studio perisien au coeur du Marais, etc text',
+      hotelTitle: 'Entire guesthouse',
+      guest: '2',
+      studio: true,
+      beds: '1',
+      bath: '1',
+      rate: '4.73',
+      quantityRate: '202',
+      price: 'R$365',
+      tags: [{name: 'wifi'}, {name: 'kitchen'}, {name: 'Heating'}]
+    },
+    {
+      plus: false,
+      image: 'https://a0.muscache.com/im/pictures/miso/Hosting-19140555/original/bbbde631-db06-4171-9283-d0f2b2a224cf.jpeg?im_w=480',
+      type: 'Entire guesthouse',
+      title: 'Champs-Élysées Golden triangle flat with mezzanine',
+      hotelTitle: 'Entire guesthouse',
+      guest: '2',
+      studio: true,
+      beds: '0',
+      bath: '1',
+      rate: '4.61',
+      quantityRate: '194',
+      price: 'R$365',
+      tags: [{name: 'wifi'}, {name: 'kitchen'}, {name: 'Heating'}]
+    },
+  ];
+
+  constructor(
+    public matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer,
+  ) {
+    matIconRegistry.addSvgIcon(
+      'star',
+      domSanitizer.bypassSecurityTrustResourceUrl('assets/star.svg')
+    );
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,23 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
+import {CommonModule} from "@angular/common";
 import {MatCardModule} from "@angular/material/card";
+import {HttpClientModule} from "@angular/common/http";
+import {MatChipsModule} from "@angular/material/chips";
+import {MatInputModule} from "@angular/material/input";
 import {MatSelectModule} from "@angular/material/select";
-import {MatNativeDateModule} from "@angular/material/core";
-import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatButtonModule} from "@angular/material/button";
+import { BrowserModule } from '@angular/platform-browser';
 import {MatDividerModule} from "@angular/material/divider";
 import {MatTooltipModule} from "@angular/material/tooltip";
-import {MatExpansionModule} from "@angular/material/expansion";
-import {MatMenuModule} from "@angular/material/menu";
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatListModule} from "@angular/material/list";
-import {MatIconModule} from "@angular/material/icon";
-import {MatSidenavModule} from "@angular/material/sidenav";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {MatSnackBarModule} from "@angular/material/snack-bar";
-import {CommonModule} from "@angular/common";
-import {HttpClientModule} from "@angular/common/http";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {MatProgressBarModule} from "@angular/material/progress-bar";
 import {MatFormFieldModule} from "@angular/material/form-field";
-import {MatButtonModule} from "@angular/material/button";
-import {MatInputModule} from "@angular/material/input";
-import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
+import { AppComponent } from './app.component';
+
+import { HeaderComponent } from './shared/header/header.component';
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
@@ -43,16 +35,18 @@ import {MatSlideToggleModule} from "@angular/material/slide-toggle";
     FormsModule,
 
     MatCardModule,
-    MatFormFieldModule,
-    MatButtonModule,
     MatInputModule,
+    MatChipsModule,
+    MatButtonModule,
+    MatFormFieldModule,
     MatSlideToggleModule,
     BrowserAnimationsModule,
 
     MatTooltipModule,
     // BrowserModule.withServerTransition({appId: 'serverApp'}),
     MatSelectModule,
-    MatDividerModule
+    MatDividerModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
