158c8f942028e0b0d436515cabd5bf4b442924fd
rober <robert.nunes@evo1.com.br>
2021-09-23 00:21:34 -0300

    map

---
+++ src/app/shared/components/map-button/map-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-map-button',
+  templateUrl: './map-button.component.html',
+  styleUrls: ['./map-button.component.css']
+})
+export class MapButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
